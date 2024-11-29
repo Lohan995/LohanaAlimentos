17a8dc75599818a6c70b53327f6d2d0398fa868a
japinhaharuu <00001071195025sp@al.educacao.sp.gov.br>
2024-11-28 22:27:43 -0300

    Initial commit

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Comida',
+    'Qual é o benefício principal da maçã?',
+    'A maçã é rica em fibras e antioxidantes, ajudando na digestão e saúde do coração'
+)
+
+criaCartao(
+    'Comida',
+    'Qual é a principal diferença entre batata doce e batata comum?',
+    'A batata doce tem mais fibras e é uma fonte de carboidratos de baixo índice glicêmico'
+)
+
+criaCartao(
+    'Comida',
+    'O que é a quinoa?',
+    'A quinoa é um grão rico em proteínas e sem glúten, frequentemente usado em dietas vegetarianas'
+)
+
+criaCartao(
+    'Comida',
+    'Como o abacate pode ser benéfico para a saúde?',
+    'O abacate é rico em gorduras saudáveis, como o ácido oleico, que é bom para o coração'
+)
+
+criaCartao(
+    'Comida',
+    'Qual é a diferença entre leite integral e leite desnatado?',
+    'O leite desnatado tem menos gordura, pois a gordura do leite é removida durante o processo'
+)
